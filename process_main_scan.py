f7b2df9e6b9221dcd73a6a8db95490b216f1927b
miro1501 <mirosrednoselec@yahoo.com>
2017-09-09 22:22:26 +0200

    checking if input and output directory exists in one function

--- process_main_scan.py
+++ process_main_scan.py
@@ -6,8 +6,12 @@ def get_args():
     parser = argparse.ArgumentParser(
         description='Scan the folder for Srednoselecs new pictures and videos',
     )
+
     parser.add_argument(
-        '-s', '--dir', type=str, help='Direcory for scan', required=True)
+        '-i', '--input_dir', type=str, help='Direcory for scan. Input directory', required=True)
+    parser.add_argument(
+        '-o', '--output_dir', type=str, help='Destination directory. Output directory', required=True)
+
     args = parser.parse_args()
     return args
 
@@ -31,16 +35,23 @@ def check_crea_month_year(files):
         files_infomation.append(tmp_list)
     return files_infomation
 
-def main():
-    dirname = get_args().dir
-    list_of_files = []
-    if os.path.isdir(dirname):
-        list_of_files = list_all_files_in_dir(dirname)
-    else:
-        print("Given arg is <{}> is not directory".format(dirname))
+def check_if_dir(input_dir, output_dir):
+    if not os.path.isdir(input_dir):
+        print("Given arg1 (input_dir or -i) is <{}> is not directory".format(input_dir))
+        exit(0)
+    if not os.path.isdir(input_dir):
+        print("Given arg2 (output_dir or -o) is <{}> is not directory".format(output_dir))
+        exit(0)
 
+
+
+def main():
+    input_dirname = get_args().input_dir
+    output_dirname = get_args().output_dir
+    check_if_dir(input_dirname, output_dirname)
+    list_of_files = list_all_files_in_dir(input_dirname)
     all_informations_of_the_files = check_crea_month_year(list_of_files)
-    print(all_informations_of_the_files)
+
 
 if __name__ == "__main__":
     main()
