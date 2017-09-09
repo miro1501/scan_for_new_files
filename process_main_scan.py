acc73877c6355c038eebbd4354dc35cb1e97b418
miro1501 <mirosrednoselec@yahoo.com>
2017-09-09 21:55:28 +0200

    following functions are implemented: -reading the files from one folder -getting the year/month of creation date

--- process_main_scan.py
+++ process_main_scan.py
@@ -1,8 +1,6 @@
 import argparse
 import os
-from os.path import isfile
-import sys
-
+import time
 
 def get_args():
     parser = argparse.ArgumentParser(
@@ -13,21 +11,36 @@ def get_args():
     args = parser.parse_args()
     return args
 
-def list_all_files_in_dir(dirname):
-    dirs = os.listdir(dirname)
-    for file in dirs:
-        if os.path.isfile(file):
-            print("Found file",file)
 
+def list_all_files_in_dir(dirname):
+    list_of_files = []
+    dirs = os.listdir(dirname)
+    for files in dirs:
+        list_of_files.append(dirname + '/' + files)
+    return list_of_files
+
+def check_crea_month_year(files):
+    files_infomation = []
+    for file in files:
+        tmp_list = []
+        year = time.strftime('%Y', time.gmtime(os.path.getmtime(file)))
+        month = time.strftime('%m', time.gmtime(os.path.getmtime(file)))
+        tmp_list.append(file)
+        tmp_list.append(year)
+        tmp_list.append(month)
+        files_infomation.append(tmp_list)
+    return files_infomation
 
 def main():
     dirname = get_args().dir
+    list_of_files = []
     if os.path.isdir(dirname):
-        list_all_files_in_dir(dirname)
+        list_of_files = list_all_files_in_dir(dirname)
     else:
         print("Given arg is <{}> is not directory".format(dirname))
 
-
+    all_informations_of_the_files = check_crea_month_year(list_of_files)
+    print(all_informations_of_the_files)
 
 if __name__ == "__main__":
     main()
