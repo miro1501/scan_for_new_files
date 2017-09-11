0710086f348ee6ecd32db4869fd7083d408336c3
miro1501 <31803861+miro1501@users.noreply.github.com>
2017-09-11 15:09:08 +0200

    Update copy_move_files.py
    new move action

--- helper_output/copy_move_files.py
+++ helper_output/copy_move_files.py
@@ -4,4 +4,10 @@ import shutil
 def copy_files(file, source_dir_path, destination_dir_path):
     source_file_path = source_dir_path + '/' + file
     destination_file_path = destination_dir_path + '/' + file
+    shutil.copyfile(source_file_path, destination_file_path)
+
+
+def move_files(file, source_dir_path, destination_dir_path):
+    source_file_path = source_dir_path + '/' + file
+    destination_file_path = destination_dir_path + '/' + file
     shutil.move(source_file_path, destination_file_path)
