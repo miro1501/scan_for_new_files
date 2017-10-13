66fad51ca0f80c505f7aae8d6cca85c9faf9ff52
miro1501 <mirosrednoselec@yahoo.com>
2017-10-13 14:08:57 +0200

    PIL library for the create date for pictures imported

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 setup(
-    name="scan_for_new_files",
-    version=__version__,
-    packages=find_packages(),
-    install_requires=["pillow"]
+    name='scan_for_new_files',
+    version='',
+    packages=['Pillow>=2.0.0'],
+    url='',
+    license='',
+    author='Miro Srednoselec',
+    author_email='',
+    description=''
 )
