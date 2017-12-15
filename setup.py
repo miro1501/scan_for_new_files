74c5590185bb7a2ff9906c3c87945583f4cb8f14
mirosrednoselec <mirosrednoselec@googlemail.com>
2017-12-15 11:04:35 -0500

    setup changed

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
+# following libs are needed:
+# sudo apt-get install zlib1g-dev
+# sudo apt-get install libjpeg-dev
 from setuptools import setup
 
 setup(
     name='scan_for_new_files',
-    packages = ['scan_for_new_files']
+    packages = ['scan_for_new_files'],
+    install_requires = ['Pillow >= 2.0.0']
 )
