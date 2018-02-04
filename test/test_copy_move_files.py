78ed6d1233415d3c957584d8ff5dacea8ba48a5f
mirosrednoselec <mirosrednoselec@yahoo.com>
2018-02-04 13:57:57 -0500

    new test test_copy_move_files

--- test/test_copy_move_files.py
+++ test/test_copy_move_files.py
@@ -1,23 +1,33 @@
-import unittest, os
-import shutil, tempfile
+import unittest, os, shutil
 from scan_for_new_files.copy_move_files import CopyMoveFiles
 
 
-class CopyMoveFilesTestCase( unittest.TestCase ):
-	def setUp(self):
-	    #create temp direcroties
-	    self.test_dir_1 = tempfile.mkdtemp()
-	    self.test_dir_2 = tempfile.mkdtemp()
-	    #create one temp file
-	    self.file = tempfile.NamedTemporaryFile(dir=self.test_dir_1)
-	    print(self.test_dir_1)
-	    print(self.test_dir_2)
-	    print(self.file)
+class CopyMoveFilesTestCase(unittest.TestCase):
+    @classmethod
+    def setUp(self):
+        print("setUp")
+        self.dir1 = "dir1"
+        os.mkdir(self.dir1)
+        self.dir2 = "dir2"
+        os.mkdir(self.dir2)
+        self.file1 = "test_file1"
+        self.file2 = "test_file2"
+        os.mknod(self.dir1 + "/test_file1")
+        os.mknod(self.dir2 + "/test_file2")				
 
-	def tearDown(self):
-	    #remove the directories after the test
-            shutil.rmtree(self.test_dir_1)		
-            shutil.rmtree(self.test_dir_2)		
+    def test_move_file(self):
+        print("test_move_file")			
+        CopyMoveFiles.move_files(self.file1, self.dir1, self.dir2)
+        self.assertEqual(os.path.isfile("dir2/test_file1"), True)
 
-	def test_copy_file(self):
-	    CopyMoveFiles.move_files(self.file, self.test_dir_1, self.test_dir_2)
+    def test_copy_file(self):
+        print("test_copy_file")			
+        CopyMoveFiles.copy_files(self.file2, self.dir2, self.dir1)
+        self.assertEqual(os.path.isfile("dir1/test_file2"), True)
+
+    @classmethod
+    def tearDown(self):
+        # remove the directories after the test
+        print("teardown")
+        shutil.rmtree(self.dir1)
+        shutil.rmtree(self.dir2)
